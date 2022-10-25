9c0fc8a026e86aab1e3a94a0de18264c663f96ac
z80 <z80@ophy.xyz>
2022-10-24 20:50:54 -0400

    add vyper support

--- test/SimpleStore.t.sol
+++ test/SimpleStore.t.sol
@@ -4,22 +4,30 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
+import "utils/VyperDeployer.sol";
 
 contract SimpleStoreTest is Test {
     /// @dev Address of the SimpleStore contract.
     SimpleStore public simpleStore;
+    SimpleStore public vyperStore;
+    VyperDeployer vyperDeployer = new VyperDeployer();
+
 
     /// @dev Setup the testing environment.
     function setUp() public {
         simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+        vyperStore = SimpleStore(vyperDeployer.deployContract("SimpleStore"));
     }
 
     /// @dev Ensure that you can set and get the value.
     function testSetAndGetValue(uint256 value) public {
         simpleStore.setValue(value);
+        vyperStore.setValue(value);
         console.log(value);
         console.log(simpleStore.getValue());
+        console.log(vyperStore.getValue());
         assertEq(value, simpleStore.getValue());
+        assertEq(simpleStore.getValue(), vyperStore.getValue());
     }
 }
 
